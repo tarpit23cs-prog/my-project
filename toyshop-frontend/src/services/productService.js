27711e855e2eeb98ce2faa298e1784e88ad12b98
arpit tiwari <tarpit23cs@student.mes.ac.in>
2026-01-30 23:43:00 +0530

    fix api url

--- toyshop-frontend/src/services/productService.js
+++ toyshop-frontend/src/services/productService.js
@@ -1,6 +1,7 @@
-const BASE_URL = `${import.meta.env.VITE_API_URL}/api/products`;
 
+const API = import.meta.env.VITE_API_URL;
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/products`;
 export const getProducts = async () => {
-  const res = await fetch(BASE_URL);
+  const res = await fetch(`${API}/api/products`);
   return res.json();
 };
