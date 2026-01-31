9e2b00098a9f0f3f075d08321c2e63596c58c56d
arpit tiwari <tarpit23cs@student.mes.ac.in>
2026-01-31 12:02:17 +0530

    removed context

--- toyshop-frontend/src/main.jsx
+++ toyshop-frontend/src/main.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import { CartProvider } from "./context/CartContext";
-
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
+    <App />
   </BrowserRouter>
 );
